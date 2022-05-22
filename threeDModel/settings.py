d856d60c24ba9ee7b3a30d19513282792e995f89
M S Shiraj <msshiraj1999@gmail.com>
2022-05-22 21:32:34 +0530

    Settings.py updated

--- threeDModel/settings.py
+++ threeDModel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_@$aexqvcsinwculwdata$%yhrolcym)0=%8w#srk$nvv_$ndt
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['mrmapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
